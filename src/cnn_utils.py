dd9314106d1520f3892c961c8c0d9231a6691f26
Xudong Yang <szyangxd@gmail.com>
2021-05-17 10:37:47 -0700

    add source file for function import, upload notebook on first try of inception model

---
+++ src/cnn_utils.py
@@ -0,0 +1,136 @@
+import torch
+import torchvision
+from torch import nn, optim
+import numpy as np
+import pandas as pd
+from captum.attr import GradientShap
+from captum.attr import visualization as viz
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d):
+        print("Initializing weights of a Conv2d layer!")
+        nn.init.normal_(m.weight, mean=0, std=0.1)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+    elif isinstance(m, nn.BatchNorm2d): 
+        print("Initializing weights of a batchnorm layer!")
+        nn.init.ones_(m.weight)
+        nn.init.zeros_(m.bias)
+    elif isinstance(m, nn.Linear):
+        print("Initializing weights of a Linear layer!")
+        nn.init.xavier_uniform_(m.weight)
+        nn.init.zeros_(m.bias)
+        
+def trainer(model, criterion, optimizer, train_loader, valid_loader, device, epochs=5, verbose=True):
+    """Simple training wrapper for PyTorch network."""
+    
+    train_accuracy = []
+    valid_accuracy = []
+    for epoch in range(epochs):  # for each epoch
+        train_batch_loss = 0
+        train_batch_acc = 0
+        valid_batch_loss = 0
+        valid_batch_acc = 0
+        
+        # Training
+        for X, y in train_loader:
+            if device.type == 'cuda':
+                X, y = X.to(device), y.to(device)
+            optimizer.zero_grad()
+            y_hat = model(X).flatten()
+            y_hat_labels = torch.sigmoid(y_hat) > 0.5
+            loss = criterion(y_hat, y.type(torch.float32))
+            loss.backward()
+            optimizer.step()
+            train_batch_loss += loss.item()
+            train_batch_acc += (y_hat_labels == y).type(torch.float32).mean().item()
+        train_accuracy.append(train_batch_acc / len(train_loader))
+        
+        # Validation
+        confusion_matrix = torch.zeros(2, 2)
+        model.eval()
+        with torch.no_grad():
+            for X, y in valid_loader:
+                if device.type == 'cuda':
+                    X, y = X.to(device), y.to(device)
+                y_hat = model(X).flatten()
+                y_hat_labels = torch.sigmoid(y_hat) > 0.5
+                loss = criterion(y_hat, y.type(torch.float32))
+                valid_batch_loss += loss.item()
+                valid_batch_acc += (y_hat_labels == y).type(torch.float32).mean().item()
+        valid_accuracy.append(valid_batch_acc / len(valid_loader))
+        
+        with torch.no_grad():
+            for i, (inputs, classes) in enumerate(valid_loader):
+                inputs = inputs.to(device)
+                classes = classes.to(device)
+                outputs = model(inputs).flatten()
+                preds = torch.sigmoid(outputs) > 0.5
+                for t, p in zip(classes.view(-1), preds.view(-1)):
+                        confusion_matrix[t.long(), p.long()] += 1
+        recall = confusion_matrix.numpy()[1,1]/(confusion_matrix.numpy()[1,1]+confusion_matrix.numpy()[1,0])
+        model.train()
+        
+        # Print progress
+        if verbose:
+            print(f"Epoch {epoch + 1}:",
+                  f"Train Accuracy: {train_accuracy[-1]:.2f}.",
+                  f"Valid Accuracy: {valid_accuracy[-1]:.2f}.",
+                  f"Valid Recall: {recall:.2f}")
+    
+    return {"train_accuracy": train_accuracy, "valid_accuracy": valid_accuracy, "Valid_recall":recall}
+
+def cnn_feature_importance(cnn, image_tensor):
+    torch.manual_seed(2020); np.random.seed(2020)
+    cnn = cnn.cpu()
+    gradient_shap = GradientShap(cnn)
+    rand_img_dist = torch.cat([image_tensor * 0, image_tensor * 1])
+    attributions_gs = gradient_shap.attribute(image_tensor,
+                                              n_samples=20,
+                                              stdevs=0.15,
+                                              baselines=rand_img_dist,
+                                              target=0)
+    _ = viz.visualize_image_attr_multiple(np.transpose(attributions_gs.squeeze().detach().numpy(), (1,2,0)),
+                                        np.transpose(image_tensor.squeeze().detach().numpy(), (1,2,0)),
+                                        ["original_image", "blended_heat_map"],
+                                        ["all", "absolute_value"],
+                                        titles = ["Original Image", "Gradient SHAP"],
+                                        cmap="plasma",
+                                        show_colorbar=True,
+                                        fig_size=(6, 6),
+                                        alpha_overlay=0.7)
+
+def get_test_accuracy(cnn, data_loader):
+    test_accuracy = []
+    cnn = cnn.cuda()
+    test_batch_acc = 0
+    cnn.eval()
+    with torch.no_grad():
+        for X, y in data_loader:
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            if device.type == 'cuda':
+                X, y = X.to(device), y.to(device)
+            y_hat = cnn(X).flatten()
+            y_hat_labels = torch.sigmoid(y_hat) > 0.5
+            test_batch_acc += (y_hat_labels == y).type(torch.float32).mean().item()
+    test_accuracy.append(test_batch_acc / len(data_loader))
+    return test_accuracy[-1] 
+
+def pytorch_confusion_matrix(nb_classes, cnn, data_loader):
+
+    confusion_matrix = torch.zeros(nb_classes, nb_classes)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    with torch.no_grad():
+        for i, (inputs, classes) in enumerate(data_loader):
+            inputs = inputs.to(device)
+            classes = classes.to(device)
+            outputs = cnn(inputs).flatten()
+            preds = torch.sigmoid(outputs) > 0.5
+            for t, p in zip(classes.view(-1), preds.view(-1)):
+                    confusion_matrix[t.long(), p.long()] += 1
+
+    cm = pd.DataFrame(confusion_matrix.numpy(), columns = ['predict negative','predict positive'], index = ['actual negative', 'actual positive'])
+    return cm
+
+def recall_calculation(confusion_matrix):
+    return cmtx.iloc[1,1]/(cmtx.iloc[1,1]+cmtx.iloc[1,0])
